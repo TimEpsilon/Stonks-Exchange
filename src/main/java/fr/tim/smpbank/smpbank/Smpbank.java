d93cb5110a1fa324d15cc8bb9108e937a3462a44
BuildTools <unconfigured@null.spigotmc.org>
2021-03-27 17:58:15 +0100

    Initial commit

---
+++ src/main/java/fr/tim/smpbank/smpbank/Smpbank.java
@@ -0,0 +1,17 @@
+package fr.tim.smpbank.smpbank;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Smpbank extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
