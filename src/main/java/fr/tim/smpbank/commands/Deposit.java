588757096db439e75cfb641310399c866a7ad632
Tim91690 <tim.dewachter@live.fr>
2021-04-22 02:09:00 +0200

    recuperation de la banque avec config + début commande deposit
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/java/fr/tim/smpbank/commands/Deposit.java
+++ src/main/java/fr/tim/smpbank/commands/Deposit.java
@@ -1,4 +1,29 @@
 package fr.tim.smpbank.commands;
 
-public class Deposit {
+import fr.tim.smpbank.bank.Trader;
+import fr.tim.smpbank.gui.Interface;
+import fr.tim.smpbank.smpBank;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Deposit implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (args.length == 0) { //pas de check si le string est un int
+                player.sendMessage(ChatColor.RED + "Veuillez entrer une valeur positive!");
+                return false;
+            }
+            int n = Integer.parseInt(args[0]);
+            Trader.deposit(n,player);
+            return true;
+        }
+        return false;
+    }
 }
